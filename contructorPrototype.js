6dde056a80e22354ce698e10e884f1ffaa7462eb
Rolando Cachipuendo <r_cachipuendo_reca@hotmail.com>
2021-05-22 17:32:14 -0500

    Constructor prototype Pattern

---
+++ contructorPrototype.js
@@ -0,0 +1,13 @@
+class MyClass {
+  constructor(p1, p2) {
+    this.property = p1;
+    this.property2 = p2;
+  }
+
+  method() {
+    //I'm a method.
+  }
+}
+
+const instance = new MyClass("2", "3");
+console.log(instance);
